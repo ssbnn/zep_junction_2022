f4f2e12c4b1f3f21e86b3bf534a6c2acd13e93e4
ssbnn <sbzzang41@gmail.com>
2022-08-21 03:44:33 +0900

    firth commit

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-let ad = App.loadSpritesheet("700.png");
+let ad = App.loadSpritesheet("hellozep.png");
 let button = App.loadSpritesheet("fixed.png");
 
-App.onJoinPlayer.Add(function(){
+
+App.onJoinPlayer.Add(function(player){
+    Map.putObject(10, 10, ad, {overlap : true});
+    Map.putObject(10, 14, button, {overlap : true});
+    player.tag = {widget: null};
     App.httpGet(
         "https://blackvod.blackwind.tech/temp",
         null,
@@ -17,20 +21,17 @@ App.onJoinPlayer.Add(function(){
             App.sayToAll(`ad_num: ${response.ad_num}`, 0xffffff);
             App.sayToAll(`img: ${response.image}`, 0xffffff);
         });
-
-    Map.putObject(10, 10, ad, {overlap : true});
-    Map.putObject(10, 14, button, {overlap : true});
 });
 
 App.addOnTileTouched(10, 10, function (player) {
-    player.spawnAtMap("65jeBA", "2YvXMJ");
+    player.spawnAtMap("65jeBA", "2YvXMJ"); //-- spacehash first, maphash second --//
 });
+
 
 App.addOnTileTouched(10, 14, function (player) {
     player.tag.widget = player.showWidget("ad.html", "top", 600, 300);
 	player.tag.widget.onMessage.Add(function (player, msg) {
 		if (msg.type == "close") {
-			player.showCenterLabel("위젯이 닫혔습니다.");
 			player.tag.widget.destroy();
 			player.tag.widget = null;
 		}
