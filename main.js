9ed26d331810964cd422e6e484ad1fdda24dd0e2
ssbnn <sbzzang41@gmail.com>
2022-08-21 00:26:09 +0900

    second commit

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let ad = App.loadSpritesheet("728.png");
+let ad = App.loadSpritesheet("700.png");
+let button = App.loadSpritesheet("fixed.png");
+let _widget = null;
 var response;
 
 App.onJoinPlayer.Add(function(){
@@ -20,10 +22,36 @@ App.onJoinPlayer.Add(function(){
     );
 
     Map.putObject(10, 10, ad, {overlap : true});
-    Map.putObject(13, 13, )
+    Map.putObject(10, 14, button, {overlap : true});
 
 });
 
 App.addOnTileTouched(10, 10, function (player) {
     player.spawnAtMap("65jeBA", "2YvXMJ");
 });
+
+App.addOnTileTouched(10, 14, function (player) {
+    _widget = player.showWidget("widget.html","top",300,300);
+	
+});
+
+// --------------------------------------------------------- //
+
+// 플레이어가 입장할 때 동작하는 함수
+App.onJoinPlayer.Add(function (player) {
+	player.tag = {
+		widget: null,
+	};
+
+	player.tag.widget = player.showWidget("widget.html", "top", 600, 500);
+	player.tag.widget.onMessage.Add(function (player, msg) {
+		// 위젯에서 App으로 'type: close'라는 메시지를 보내면 위젯을 파괴함
+		if (msg.type == "close") {
+			player.showCenterLabel("위젯이 닫혔습니다.");
+			player.tag.widget.destroy();
+			player.tag.widget = null;
+		}
+	});
+});
+
+
